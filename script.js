299b6fe1efaa1ceee4060f05ecdbab3e5cdc4129
unknown <mathis.madeira.com>
2022-03-11 11:52:29 +0100

    second push

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const poke_container = document.getElementById('poke_container');
-const pokemons_number = 600;
+const pokemons_number = 151;
 const colors ={
     fire: '#FDDFDF',
     grass: '#DEFDE0',
